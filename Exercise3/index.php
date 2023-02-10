a9a3d2d060c7a79cd856e8d0d25a6ab6a1fe9e5e
Lenikpoho Ahmed KONE <47745603+koneahmed@users.noreply.github.com>
2023-02-10 14:07:10 +0100

    update

--- Exercise3/index.php
+++ Exercise3/index.php
@@ -1,18 +1,16 @@
-
-<?php 
-function myFonct($value){
-     try {
-           if (filter_var($value, FILTER_VALIDATE_INT) === false){
-               throw new Exception("The value is not valid integer");
-           } else{
-               echo  " validation complete";
-           }
-     }catch (Exception $e){
-           echo  "Erro "  .$e->getMessage();
-     
-} finally {
-
-}
+<?php
+function myFonct($value)
+{
+    try {
+        if (filter_var($value, FILTER_VALIDATE_INT) === false) {
+            throw new Exception("The value is not valid integer");
+        } else {
+            echo  " validation complete";
+        }
+    } catch (Exception $e) {
+        echo  "Erro "  . $e->getMessage();
+    } finally {
+    }
 }
 $value = 2;
 myFonct($value);
